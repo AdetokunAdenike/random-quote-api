0504cb22b1ac73ab982d454f9504852a348cb69b
AdetokunAdenike <adetokunadenike@gmail.com>
2026-09-05 08:17:06 +0100

    feat: add ECR repository infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,21 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-  }  
+  }
 }
 
 provider "aws" {
   region = "eu-north-1"
+}
+
+resource "aws_ecr_repository" "random_quote_api" {
+  name                 = "random_quote_api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
